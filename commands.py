816d916f45f308c2fbda3b147d82759feafdd814
Aaron Freeman <aaronwfreeman@gmail.com>
2011-05-13 22:54:39 -0500

    added constants to command file

--- commands.py
+++ commands.py
@@ -1,6 +1,8 @@
 # Here you can create play commands that are specific to the module, and extend existing commands
 
 MODULE = 'play-lombok'
+VERSION = '0.10.0-BETA2'
+JDT_JAR='org.eclipse.jdt.core-3.6.0.jar'
 
 # Commands that are specific to your module
 
@@ -21,9 +23,9 @@ def before(**kargs):
     env = kargs.get("env")
 	
     if command == 'run' or command == 'test' or command == 'auto-test':
-        args.append('-javaagent:' + env["basedir"] + '/modules/lombok-0.10.0-BETA2/lib/lombok-0.10.0-BETA2.jar')
-        args.append('-Xbootclasspath/a:' + env["basedir"] + '/modules/lombok-0.10.0-BETA2/lib/lombok-0.10.0-BETA2.jar')
-        args.append('-Xbootclasspath/a:' + env["basedir"] + '/framework/lib/org.eclipse.jdt.core-3.6.0.jar')
+        args.append('-javaagent:' + env["basedir"] + '/modules/' + MODULE + '-' + VERSION + '/lib/lombok-0.10.0-BETA2.jar')
+        args.append('-Xbootclasspath/a:' + env["basedir"] + '/modules/' + MODULE + '-' + VERSION + '/lib/lombok-0.10.0-BETA2.jar')
+        args.append('-Xbootclasspath/a:' + env["basedir"] + '/framework/lib/' + JDT_JAR)
 
 
 # This will be executed after any command (new, run...)
