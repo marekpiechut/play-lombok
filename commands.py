7e56f3a7608e523a560cbe08370568d439b550b2
aaronfreeman <aaronwfreeman@gmail.com>
2011-06-01 16:06:32 -0500

    added precompile to list of commands this operates on

--- commands.py
+++ commands.py
@@ -1,6 +1,6 @@
 # Here you can create play commands that are specific to the module, and extend existing commands
 
-MODULE = 'play-lombok'
+MODULE = 'play'
 VERSION = '0.10.0-BETA2'
 JDT_JAR='org.eclipse.jdt.core-3.6.0.jar'
 
@@ -22,7 +22,7 @@ def before(**kargs):
     args = kargs.get("args")
     env = kargs.get("env")
 	
-    if command == 'run' or command == 'test' or command == 'auto-test' or command == 'start':
+    if command == 'run' or command == 'test' or command == 'auto-test' or command == 'start' or command == 'precompile':
         args.append('-javaagent:' + env["basedir"] + '/modules/' + MODULE + '-' + VERSION + '/lib/lombok-0.10.0-BETA2.jar')
         args.append('-Xbootclasspath/a:' + env["basedir"] + '/modules/' + MODULE + '-' + VERSION + '/lib/lombok-0.10.0-BETA2.jar')
         args.append('-Xbootclasspath/a:' + env["basedir"] + '/framework/lib/' + JDT_JAR)
