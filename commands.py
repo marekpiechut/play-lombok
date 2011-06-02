9457f15e9652dca051d09f9a8b6b6e2903c5b1b6
aaronfreeman <aaronwfreeman@gmail.com>
2011-06-02 14:32:14 -0500

    added deploy command

--- commands.py
+++ commands.py
@@ -4,6 +4,7 @@ MODULE = 'lombok'
 VERSION = '0.10.0-BETA2'
 JDT_JAR='org.eclipse.jdt.core-3.6.0.jar'
 LOMBOK_JAR='lombok-' + VERSION + '.jar'
+ALTERED_COMMANDS = ['run','test','auto-test','start','precompile','deploy']
 
 # Commands that are specific to your module
 
@@ -23,7 +24,7 @@ def before(**kargs):
     args = kargs.get("args")
     env = kargs.get("env")
 	
-    if command == 'run' or command == 'test' or command == 'auto-test' or command == 'start' or command == 'precompile':
+    if command in ALTERED_COMMANDS:
         args.append('-javaagent:' + os.path.join(env["basedir"], 'modules', MODULE + '-' + VERSION, 'lib', LOMBOK_JAR))
         args.append('-Xbootclasspath/a:' + os.path.join(env["basedir"], 'modules', MODULE + '-' + VERSION, 'lib', LOMBOK_JAR))
         args.append('-Xbootclasspath/a:' + os.path.join(env["basedir"], 'framework', 'lib', JDT_JAR))
