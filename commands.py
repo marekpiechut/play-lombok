fbbd7043225a3731f14df4eb867cd3568bac50c0
Aaron Freeman <aaronwfreeman@gmail.com>
2011-08-01 23:20:20 -0500

    pulling form maven now

--- commands.py
+++ commands.py
@@ -1,7 +1,7 @@
 import os, os.path
 
 MODULE = 'lombok'
-VERSION = '0.10.0-RC1'
+VERSION = '0.10.0-RC3'
 JDT_JAR='org.eclipse.jdt.core-3.6.0.jar'
 LOMBOK_JAR='lombok-' + VERSION + '.jar'
 ALTERED_COMMANDS = ['run','test','auto-test','start','precompile','deploy']
@@ -23,10 +23,10 @@ def before(**kargs):
     app = kargs.get("app")
     args = kargs.get("args")
     env = kargs.get("env")
-	
+
     if command in ALTERED_COMMANDS:
-        args.append('-javaagent:' + os.path.join(env["basedir"], 'modules', MODULE + '-' + VERSION, 'lib', LOMBOK_JAR))
-        args.append('-Xbootclasspath/a:' + os.path.join(env["basedir"], 'modules', MODULE + '-' + VERSION, 'lib', LOMBOK_JAR))
+        args.append('-javaagent:' + os.path.join(app.path, 'lib', LOMBOK_JAR))
+        args.append('-Xbootclasspath/a:' + os.path.join(app.path, 'lib', LOMBOK_JAR))
         args.append('-Xbootclasspath/a:' + os.path.join(env["basedir"], 'framework', 'lib', JDT_JAR))
 
 
